8ec6a0797541f59c8518504a7b0a2e94cd27a6b2
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2021-07-11 17:46:04 +0100

    Updated to version 0.0.5
     - Fixed error: 
       type object 'SHA1' has no attribute 'HashSaltPepper'

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CosmosHashLib",
+    version="0.0.5",
+    author="Cosmos Bots",
+    author_email="contact@cosmosbots.com",
+    description="A simple hashing wrapper with support for salts and peppers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vihangatheturtle/CosmosHashLib",
+    project_urls={
+        "Bug Tracker": "https://github.com/vihangatheturtle/CosmosHashLib/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
